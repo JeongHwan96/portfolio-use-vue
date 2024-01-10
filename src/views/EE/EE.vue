c4e3e6c1c94bc8c2f627cb758cfbf07ddd7a792b
JeongHwan96 <76175940+JeongHwan96@users.noreply.github.com>
2024-01-11 00:03:55 +0900

    upload

--- src/views/EE/EE.vue
+++ src/views/EE/EE.vue
@@ -17,7 +17,22 @@
           <hr />
           2017.01 ~ 2019.02(산업IOT 융복합 전공)
         </div>
-        <div class="company1">
+        <div class="academy1">
+          TIS 정보기술교육센터
+          <hr />
+          2021.02 ~ 2021.07(FrontEnt,BackEnd 개발자 양성과정)
+        </div>
+        <div class="academy2">
+          코멘토 직무부트캠프
+          <hr />
+          2022.02 ~ 2022.03
+        </div>
+        <div class="academy3">
+          메가스터디IT아케데미
+          <hr />
+          2022.06 ~ 2022.10
+        </div>
+        <!-- <div class="company1">
           와이즈스톤
           <hr />
           2018.11 ~ 2020.09
@@ -28,7 +43,7 @@
           <hr />
           2020.09 ~ 2020.11
           <p>삼성생명 홈페이지 리뉴얼 작업</p>
-        </div>
+        </div> -->
       </div>
     </div>
   </div>
